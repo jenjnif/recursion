69c2eaf7a8606d57cec37139d60d886b09d35609
jenjnif <jenjnif@hotmail.com>
2019-02-04 16:18:40 +0000

    Add test 2d array with 2 rows and 2 columns

--- min_sum_route.py
+++ min_sum_route.py
@@ -2,17 +2,23 @@
 # and you can only move down and right.
 
 single_arr = [1]
-two_dimensional_arr = [[2, 3], [1, 2]]
+four_arr = [[1,8], [2, 3]]
+# two_dimensional_arr = [[2, 3], [1, 2]]
 
 
-def least_sum_route(single_arr):
+def least_sum_route(arr):
+    # base case
     if len(single_arr) == 1:
         return single_arr
+    # recursive case
 
 
-# TESTS
-
+'''   TESTS   '''
 # first test reduce to single cell and this is base case?
 # If len(list) == 1: return list
 def test_single_cell():
     assert least_sum_route(single_arr) == [1]
+
+
+def test_four_cell():
+    assert least_sum_route(four_arr) == 6
