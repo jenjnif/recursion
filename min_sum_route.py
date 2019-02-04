688dc562c4645965eb6a61281851bb8f7675c9be
jenjnif <jenjnif@hotmail.com>
2019-02-04 15:29:09 +0000

    First commit

---
+++ min_sum_route.py
@@ -0,0 +1,14 @@
+# find the least sum path from top left to bottom right
+# and you can only move down and right.
+
+
+arr = [[2, 3], [1, 2]]
+
+def least_sum_route ():
+    pass
+
+
+# TESTS
+
+# first test reduce to single cell and this is base case?
+#If len(list) == 1: return list
