a95ebb52832ca6fea69780a52059b3dac5fc560c
jenjnif <jenjnif@hotmail.com>
2019-02-04 15:57:26 +0000

    Add first test for single item in array

--- min_sum_route.py
+++ min_sum_route.py
@@ -1,14 +1,17 @@
 # find the least sum path from top left to bottom right
 # and you can only move down and right.
 
+single_arr = [1]
+two_dimensional_arr = [[2, 3], [1, 2]]
 
-arr = [[2, 3], [1, 2]]
 
-def least_sum_route ():
+def least_sum_route(single_arr):
     pass
 
 
 # TESTS
 
 # first test reduce to single cell and this is base case?
-#If len(list) == 1: return list
+# If len(list) == 1: return list
+def test_single_cell():
+    assert least_sum_route(single_arr) == [1]
