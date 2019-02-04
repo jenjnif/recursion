d0cb2a1b5282dcc4df50456621422ee6092ed098
jenjnif <jenjnif@hotmail.com>
2019-02-04 16:45:38 +0000

    Update second test which number is smaller

--- min_sum_route.py
+++ min_sum_route.py
@@ -6,12 +6,20 @@ four_arr = [[1,8], [2, 3]]
 # two_dimensional_arr = [[2, 3], [1, 2]]
 
 
+def is_smaller(x, y):
+    pass
+
+
 def least_sum_route(arr):
     # base case
-    if len(single_arr) == 1:
-        return single_arr
+    if len(arr) == 1:
+        return arr
     # recursive case
+    else:
+        pass
 
+
+# print(least_sum_route(four_arr))
 
 '''   TESTS   '''
 # first test reduce to single cell and this is base case?
@@ -20,5 +28,5 @@ def test_single_cell():
     assert least_sum_route(single_arr) == [1]
 
 
-def test_four_cell():
-    assert least_sum_route(four_arr) == 6
+def test_is_number_smaller():
+    assert is_smaller(four_arr) == 2
