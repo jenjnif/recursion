4b1e4e3f1d45e21c21abd71ef2cdb3d48770ff16
jenjnif <jenjnif@hotmail.com>
2019-02-05 08:55:58 +0000

    Change problem to find lowest sum of routes

--- min_sum_route.py
+++ min_sum_route.py
@@ -1,32 +1,22 @@
 # find the least sum path from top left to bottom right
 # and you can only move down and right.
 
-single_arr = [1]
-four_arr = [[1,8], [2, 3]]
-# two_dimensional_arr = [[2, 3], [1, 2]]
+arr_four = [[1,8], [2, 3]]
+# arr_six = [[1, 8, 2], [2, 3, 1], [1, 4, 1]]
 
 
-def is_smaller(x, y):
+def least_sum_route(x, y):
     pass
 
 
-def least_sum_route(arr):
-    # base case
-    if len(arr) == 1:
-        return arr
-    # recursive case
-    else:
-        pass
-
-
-# print(least_sum_route(four_arr))
-
 '''   TESTS   '''
-# first test reduce to single cell and this is base case?
+# first test reduce to single cell (final cell) and this is base case?
 # If len(list) == 1: return list
+
+
 def test_single_cell():
-    assert least_sum_route(single_arr) == [1]
+    assert least_sum_route(x, y) == [1]
 
 
-def test_is_number_smaller():
-    assert is_smaller(four_arr) == 2
+# def test_is_number_smaller():
+#     assert is_smaller((8, 2)) == 2
